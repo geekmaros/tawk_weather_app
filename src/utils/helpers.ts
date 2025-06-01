32aae977ed74e5ac2af84aa1f197fc3e671c462a
Abdulrasaq Mustapha <razicruz@gmail.com>
2025-06-01 04:52:09 +0100

    feat: add helpers

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,3 +1,11 @@
 export const sleep = (ms: number): Promise<void> => {
   return new Promise((resolve) => setTimeout(resolve, ms))
 }
+
+export const getWeatherIconUrl = (iconCode: string, size: '2x' | '4x' = '2x'): string => {
+  return `https://openweathermap.org/img/wn/${iconCode}@${size}.png`
+}
+
+export const getCountryName = (code: string) => {
+  return new Intl.DisplayNames(['en'], { type: 'region' }).of(code)
+}
